d3c5eb30bfef44b89b9a55631ed36ca308b241fd
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-02 14:17:48 -0600

    Created AddMethodTest; creates new BinarySearchTree tree, adds nodes of value 3,1, and 23. Searches for value 23 in tree and stores to Node result, reslult.data is assigned to actual variable. Checks to see if actual equals expected.

--- BinarySearchTreeUnitTesting/AddMethodTests.cs
+++ BinarySearchTreeUnitTesting/AddMethodTests.cs
@@ -7,9 +7,25 @@ namespace BinarySearchTreeUnitTesting
     [TestClass]
     public class AddMethodTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        public void Add_ThreeNodesDifferentValues_Node23ProperlyAdded()
         {
+            BinarySearchTree tree = new BinarySearchTree();
+            Node node1 = new Node(3);
+            Node node2 = new Node(1);
+            Node node23 = new Node(23);
+            Node result;
+
+            int expected = 23;
+            int actual;
+
+            tree.Add(node1);
+            tree.Add(node2);
+            tree.Add(node23);
+
+            result = tree.Search(23);
+            actual = result.data;
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
