9d5ff008a29d9cb6d4eb8731ed391d44153cee6d
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-02 14:37:07 -0600

    Created SearchMethodTest; adds three Nodes with different values to BinarySearchTree tree, searches tree for matching value and takes null Node as result, attempts to assign result.data to int actual. Throws NullReferenceException.

--- BinarySearchTreeUnitTesting/SearchMethodTests.cs
+++ BinarySearchTreeUnitTesting/SearchMethodTests.cs
@@ -28,5 +28,23 @@ namespace BinarySearchTreeUnitTesting
 
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void Search_Add3Nodes_ExceptionThrown()
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+            Node node1 = new Node(3);
+            Node node2 = new Node(1);
+            Node node23 = new Node(23);
+            Node result;
+            int actual;
+
+            tree.Add(node1);
+            tree.Add(node2);
+            tree.Add(node23);
+
+            result = tree.Search(0);
+            actual = result.data;
+        }
     }
 }
