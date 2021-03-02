689dc917817021413fbfa6261d72ce83ebaebfbf
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-02 13:15:34 -0600

    Created BinarySearchTreeUnitTesting Namespace with a reference to BinarySearchTree Namespace; Created empty AddMethodTests and SearchMethodTests Classes

---
+++ BinarySearchTreeUnitTesting/AddMethodTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BinarySearchTree;
+
+namespace BinarySearchTreeUnitTesting
+{
+    [TestClass]
+    public class AddMethodTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BinarySearchTreeUnitTesting/SearchMethodTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BinarySearchTree;
+
+namespace BinarySearchTreeUnitTesting
+{
+    [TestClass]
+    public class SearchMethodTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
